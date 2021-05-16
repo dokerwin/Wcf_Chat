4ea778e2558465a69c0bf3ddf72a6b9e48c2ca2c
ku zakhar <nik.name.doker@gmail.com>
2021-05-16 09:36:22 +0200

    Wcf_chat v1

---
+++ wcf_chat/ServerUser.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceModel;
+using System.Data;
+
+
+namespace wcf_chat
+{
+    public class ServerUser
+    {
+        private int mes;
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public OperationContext operationContext { get; set; }
+    }
+}
